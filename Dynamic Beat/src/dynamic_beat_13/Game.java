b34ce7a9cdac059cae8fe558bb653b470ca9c9db
Eight-Corner <corner3499@gmail.com>
2020-10-28 13:19:31 +0900

    Dynamic Beat project

---
+++ Dynamic Beat/src/dynamic_beat_13/Game.java
@@ -0,0 +1,168 @@
+package dynamic_beat_13;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+
+import dynamic_beat_3.Main;
+
+public class Game extends Thread {
+
+	private Image noteRouteLineImage = new ImageIcon(Main.class.getResource("../images/noteRouteLine.png")).getImage();
+	private Image judgementLineImage = new ImageIcon(Main.class.getResource("../images/judgementLine.png")).getImage();
+	private Image gameInfoImage = new ImageIcon(Main.class.getResource("../images/gameInfo.png")).getImage();
+	private Image noteRouteAImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteSImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteDImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteSpace1Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteSpace2Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteNum1Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteNum2Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	private Image noteRouteNum3Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+
+	private String titleName;
+	private String difficulty;
+	private String musicTitle;
+	private Music gameMusic;
+
+	ArrayList<Note> noteList = new ArrayList<Note>();
+
+	public Game(String titleName, String difficulty, String musicTitle) {
+		this.titleName = titleName;
+		this.difficulty = difficulty;
+		this.musicTitle = musicTitle;
+		gameMusic = new Music(this.musicTitle, false);
+		gameMusic.start();
+		dropNotes(titleName);
+	}
+
+	public void screenDraw(Graphics2D g) {
+		g.drawImage(noteRouteAImage, 228, 30, null);
+		g.drawImage(noteRouteSImage, 332, 30, null);
+		g.drawImage(noteRouteDImage, 436, 30, null);
+		g.drawImage(noteRouteSpace1Image, 540, 30, null);
+		g.drawImage(noteRouteSpace2Image, 640, 30, null);
+		g.drawImage(noteRouteNum1Image, 744, 30, null);
+		g.drawImage(noteRouteNum2Image, 848, 30, null);
+		g.drawImage(noteRouteNum3Image, 952, 30, null);
+		g.drawImage(noteRouteLineImage, 224, 30, null);
+		g.drawImage(noteRouteLineImage, 328, 30, null);
+		g.drawImage(noteRouteLineImage, 432, 30, null);
+		g.drawImage(noteRouteLineImage, 536, 30, null);
+		g.drawImage(noteRouteLineImage, 740, 30, null);
+		g.drawImage(noteRouteLineImage, 844, 30, null);
+		g.drawImage(noteRouteLineImage, 948, 30, null);
+		g.drawImage(noteRouteLineImage, 1052, 30, null);
+
+		g.drawImage(gameInfoImage, 0, 660, null);
+		g.drawImage(judgementLineImage, 0, 580, null);
+		for (int i = 0; i < noteList.size(); i++) {
+			Note note = noteList.get(i);
+			note.screenDraw(g);
+		}
+		g.setColor(Color.white);
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g.setFont(new Font("Arial", Font.BOLD, 30));
+		g.drawString(titleName, 20, 702); // 곡에 대한 정보
+		g.drawString(difficulty, 1190, 702);
+		g.setColor(Color.DARK_GRAY);
+		g.drawString("A", 270, 609);
+		g.drawString("S", 374, 609);
+		g.drawString("D", 478, 609);
+		g.drawString("Space Bar", 580, 609);
+		g.drawString("NUM1", 755, 609);
+		g.drawString("NUM2", 860, 609);
+		g.drawString("NUM3", 960, 609);
+
+		g.setFont(new Font("Elephant", Font.BOLD, 30));
+		g.drawString("000000", 565, 702);
+
+	}
+
+	public void pressA() {
+		noteRouteAImage = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumSmall1.mp3", false).start();
+	}
+
+	public void releaseA() {
+		noteRouteAImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	public void pressS() {
+		noteRouteSImage = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumSmall1.mp3", false).start();
+	}
+
+	public void releaseS() {
+		noteRouteSImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	public void pressD() {
+		noteRouteDImage = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumSmall1.mp3", false).start();
+	}
+
+	public void releaseD() {
+		noteRouteDImage = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	public void pressSpace() {
+		noteRouteSpace1Image = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		noteRouteSpace2Image = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumBig1.mp3", false).start();
+	}
+
+	public void releaseSpace() {
+		noteRouteSpace1Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+		noteRouteSpace2Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	public void pressNum1() {
+		noteRouteNum1Image = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumSmall1.mp3", false).start();
+	}
+
+	public void releaseNum1() {
+		noteRouteNum1Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	public void pressNum2() {
+		noteRouteNum2Image = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumSmall1.mp3", false).start();
+	}
+
+	public void releaseNum2() {
+		noteRouteNum2Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	public void pressNum3() {
+		noteRouteNum3Image = new ImageIcon(Main.class.getResource("../images/noteRoutePressed.png")).getImage();
+		new Music("drumSmall1.mp3", false).start();
+	}
+
+	public void releaseNum3() {
+		noteRouteNum3Image = new ImageIcon(Main.class.getResource("../images/noteRoute.png")).getImage();
+	}
+
+	@Override
+	public void run() {
+
+	}
+
+	public void close() {
+		gameMusic.close();
+		this.interrupt();
+	}
+
+	public void dropNotes(String titleName) {
+		Note note = new Note(228, "short");
+		note.start();
+		noteList.add(note);
+
+	}
+}
---
+++ Dynamic Beat/src/dynamic_beat_14/Note.java
@@ -0,0 +1,59 @@
+package dynamic_beat_14;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Note extends Thread {
+
+	private Image noteBasicImage = new ImageIcon(Main.class.getResource("../images/noteBasic.png")).getImage();
+	private int x, y = 580 - (1000 / Main.SLEEP_TIME * Main.NOTE_SPEED) * Main.REACH_TIME;
+	private String noteType;
+
+	public Note(String noteType) {
+		if(noteType.equals("A")) {
+			x = 228;
+		} else if (noteType.equals("S")) {
+			x = 332;
+		} else if (noteType.equals("D")) {
+			x = 436;
+		} else if (noteType.equals("Space")) {
+			x = 540;
+		} else if (noteType.equals("1")) {
+			x = 744;
+		} else if (noteType.equals("2")) {
+			x = 848;
+		} else if (noteType.equals("3")) {
+			x = 952;
+		}
+		this.noteType = noteType;
+	}
+	
+	public void screenDraw(Graphics2D g) {
+		if(noteType.equals("Space")) {
+			g.drawImage(noteBasicImage, x, y, null);
+			g.drawImage(noteBasicImage, x + 100, y, null);
+		}
+		else {
+			g.drawImage(noteBasicImage, x, y, null);
+
+		}
+	}
+	
+	public void drop() {
+		y += Main.NOTE_SPEED;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while(true) {
+				drop();
+				Thread.sleep(Main.SLEEP_TIME);
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
