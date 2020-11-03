b3b452b4565f13e3b39d3c5599dfa9f8721342f6
Eight-Corner <corner3499@gmail.com>
2020-11-03 23:22:19 +0900

    health project

--- Java-project/Health_Diary/src/Health_Diary/Main.java
+++ Java-project/Health_Diary/src/Health_Diary/Main.java
@@ -1,32 +1,53 @@
 package Health_Diary;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 class WorkOut {
 	Scanner sc = new Scanner(System.in);
 	String name; // 운동명 
-	int set, sec; // 운동 세트와 시간(초), 초 단위를 이용하여 시 분 초 나오게 할 예정 
-	
+	int set, cnt,sec; // 운동 세트와 횟수, 시간(초), 초 단위를 이용하여 시 분 초 나오게 할 예정 
 	WorkOut(){ // default 
 	}
-	WorkOut(String name, int set, int sec){ // 생성자 
-		this.name=name; this.set=set; this.sec=sec; 
+	WorkOut(String name, int set, int cnt,int sec){ // 생성자 
+		this.name=name; this.set=set; this.cnt=cnt; this.sec=sec; 
 	}
+ 
 	void workoutAdd () { // 운동 추가 함수 
 		System.out.println("\n\n운동을 추가하시겠습니까? (Y/N)");
 		String y = "y", Y = "Y", n = "n", N = "N"; // 결정 변수
 		String sel = sc.nextLine(); 
 		if (sel.equals(y) || sel.equals(Y)) {
-			System.out.println("\n\n운동 추가를 진행합니다. ");
+			System.out.println("\n\n 진행합니다. ");
+			System.out.println("안내:: 횟수가 아닌 초(sec)단위로 입력하고 싶으시면 "
+					+ "\n횟수 입력에서 0을 눌러 진행하십시오. ");
 			// 운동 추가 기능 구현
+			System.out.print("운동명 입력 : ");
+			this.name = sc.nextLine(); // 운동명 
 			
-		} else if ( sel.equals(n) || sel.equals(N)) {
+			System.out.print("세트 수 입력 : ");
+			this.set = sc.nextInt();
+			if (this.set <= 0 ) {
+				System.out.println("세트 수는 1 이상 입력하셔야 합니다. 다시 입력하세요.");
+				return;
+			}
+			
+			System.out.print("횟수 입력 : ");
+			this.cnt = sc.nextInt();
+			
+			if	(this.cnt == 0) {
+			System.out.print("초(sec) 입력 : ");
+			this.sec = sc.nextInt();
+			System.out.println("["+this.name+"] ["+this.set+"세트] ["+this.sec+"sec]가 저장 되었습니다.");
+				} else {
+				System.out.println("["+this.name+"] ["+this.set+"세트] ["+this.cnt+"개] 가 저장 되었습니다.");
+			}
+			
+		} 
+		else if ( sel.equals(n) || sel.equals(N)) {
 			System.out.println("이전화면으로 돌아갑니다.");
+			return;
 		} else {
 			int cls = 0;
 			for (cls = 0; cls < 15; cls++) {
@@ -45,12 +66,14 @@ class WorkOut {
 	}
 }
 
-class Memo { // 기록 클래스
+class Memo extends WorkOut{ // 기록 클래스 운동을 상속받아 운동 리스트를 호출할 예정
 	Scanner sc = new Scanner(System.in);
-	String txt;
-	private int position;
-	private Object Log;
-	void memo() {
+	int day,time; // 날짜와 운동한 시간이 필요  
+	 public Memo() {
+		 super();
+	}
+	void memo() { 
+		
 		String txt;     // 기록용 변수 선언
 		   System.out.print("기록하실 내용을 입력하세요 : ");
 			 txt = sc.nextLine(); // 기록을 하기 위한 입력값
@@ -61,16 +84,17 @@ class Memo { // 기록 클래스
 		            fw.write(txt);
 		            fw.flush();
 		            fw.close();
-		        }catch(Exception e){
+		        } catch(Exception e){
 		            e.printStackTrace();
 		        } 
 		        System.out.println();
 	}
-	void memoDel() { // 기록 특정 부분 삭제하는 함수 구현
+	void memoDel() { // 기록 특정 부분 삭제하는 함수 구현을 하고싶은데 흠흠..
 		
 	}
-	void memoView() {
-	
+	public void memoView() {
+	System.out.println("====LIST====");
+	System.out.println("["+this.day+"] ["+this.time+"] ["+this.name+"]");
 	}
 }
 
@@ -78,10 +102,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
+		
  // 다형성을 이용할 것임
-		// 생성자
+		// 생성자 
 		 WorkOut wo = new WorkOut(); 
 		Memo me = new Memo();
+		
+		//배열 구현
 		
 		while ( true ) {
 
