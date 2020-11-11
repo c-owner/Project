c77d6eb8845563eed14306c432f8b7a86fb5231c
Eight-Corner <corner3499@gmail.com>
2020-11-11 23:31:43 +0900

    -

--- MiniProject_1.5/src/WorkOut.java
+++ MiniProject_1.5/src/WorkOut.java
@@ -1,5 +1,5 @@
-public class WorkOut {  // 운동 관리 클래스
-	 int workSet; // 세트 수 
+public class WorkOut {  // 운동 관리 클래스 // 틀
+	 int workSet; // 세트 수  // 필드
 	 int workSec; // 초(시간)
 	 int workCnt; //개수 
 	 String workName; // 운동명
@@ -7,9 +7,9 @@ public class WorkOut {  // 운동 관리 클래스
 	public WorkOut (String workName, int workSet, int workCnt, int workSec) {
 		this.workName = workName; this.workSet = workSet;
 		this.workCnt = workCnt; this.workSec = workSec;
-	}
+	} // 메소드 (==기능)
 	
-	public String toString() {
+	public String toString() { // 메소드 (==기능)
 		return "["+ workName + "] ["+workSet+" 세트] ["+workCnt+" 개] ["+workSec+" 초]";
 	}
 	
