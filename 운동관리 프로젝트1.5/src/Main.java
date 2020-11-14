cc68d87e0610cd9727d3717aab2525559ee598db
Eight-Corner <corner3499@gmail.com>
2020-11-14 19:26:49 +0900

    프로젝트 이름

---
+++ 운동관리 프로젝트1.5/src/Main.java
@@ -0,0 +1,207 @@
+import java.util.Scanner;
+/*
+미니 프로젝트에 걸맞게 미니미니하게 구현
+ 발견된 버그 = 여러 기능을 사용하다 기록을 삭제 할려고 하면 삭제 실패..
+  원인 : 삭제할 파일이 사용중이라서 즉각 처리가 안되는 듯 함 ===> 윈도우만 안됨 다른 OS는 문제없었음  
+  해결 : 기록삭제는 프로그램을 재실행(Run)을 하고나서 바로 기록삭제하면 가능.
+* Patch ::
+* + 기록을 작성할 때 지정된 경로가 존재하지 않다면 디렉토리까지 폴더 생성
+* + 디렉토리 생성 
+* 
+* 디렉토리 삭제까지 하면 그 내용물까지 다 삭제되면 단점으로 다가 올 수 있어서  
+* 
+* 프로젝트 이름 변경
+*/
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		WorkoutArrayList list = new WorkoutArrayList();
+		Memo me = new Memo();
+		
+		 String y = "y",Y="Y",N="N",n="n",P="P",p="p";
+		// 자주 사용하는 선택문 클래스 선언
+		
+		 String wm; // os 선택 변수 
+		
+		boolean cnd = true;
+		while (cnd ) {
+			String os = System.getProperty(
+					"os.name").toLowerCase();
+			if (os.contains("win")) {
+				System.out.println("User OS : Windows"); 
+			} 
+			else if (os.contains("mac")) { 
+				System.out.println("User OS : Mac"); 
+			} else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) { 
+				System.out.println("User OS : Unix"); 
+			} else if (os.contains("linux")) { 
+				System.out.println("User OS : Linux");
+			} else if (os.contains("sunos")) { 
+				System.out.println("User OS : Solaris");
+			}
+
+			System.out.println("============메인 메뉴============ ");
+			System.out.print("1.운동 추가 | 2.운동 삭제 | 3.운동 조회   \n"
+							+"4.기록 조회 | 5.기록 저장 | 6.기록 삭제   \n"
+							+"7.Clear   | 0. Program EXIT....    \n");
+			System.out.println("\n===============================");
+			System.out.print("입력 >  ");
+			int sel = sc.nextInt();
+			sc.nextLine(); // 버퍼 증상 해결
+		
+			switch (sel) {
+			case 1:
+				System.out.print("추가할 운동명 : ");
+				String name = sc.next();
+				
+				System.out.print("운동의 세트 수 : ");
+				int set = sc.nextInt();
+				
+				System.out.print("몇 개 수행합니까? : ");
+				int cnt = sc.nextInt();
+				
+				System.out.print("운동의 시간(sec) : ");
+				int sec = sc.nextInt();
+
+				for (int cls = 0; cls < 15; cls++) { // 화면클리어 
+					System.out.println(); // 화면 클리어 
+					}
+				if(set <= 0 || cnt <= 0 && sec <= 0) {
+					System.out.println("WARNING : 세트 수는 1이상 입력하셔야 합니다. 처음으로 돌아갑니다. ");
+					continue;
+				}
+
+				WorkOut m = new WorkOut(name,set,cnt,sec);
+				list.addWorkout(m); // 운동 저장 
+				
+				System.out.println("성공적으로 추가를 마치셨습니다.");
+				System.out.println(m);
+				break;
+				// 
+				
+			case 2:
+				System.out.print("삭제하려는 운동의 이름을 입력 : ");
+				name = sc.nextLine();
+				
+				for (int cls = 0; cls < 15; cls++) { // 화면클리어 
+					System.out.println(); // 화면 클리어 
+					}
+				
+				list.removeWorkout(name); // 운동 삭제 함수 호출
+				System.out.println(); //줄바꿈
+				break;
+			
+			case 3:
+				for (int cls = 0; cls < 15; cls++) { // 화면클리어 
+					System.out.println(); // 화면 클리어 
+					}
+				list.showAllWorkout(); // 조회 
+				break;
+			case 4: // 기록 조회 
+				for (int cls = 0; cls < 15; cls++) { // 화면클리어 
+					System.out.println(); // 화면 클리어 
+					}
+				System.out.println("사용자의 운영체제를 파악중입니다.");
+				os = System.getProperty(
+						"os.name").toLowerCase(); 
+				if (os.contains("win")) {
+					System.out.println("사용자의 운영체제는 Windows입니다.");
+					me.memoView2(); // 윈도우 메모 
+				} else if (os.contains("mac")) {
+					System.out.println("사용자의 운영체제는 Mac OS입니다.");
+					me.memoView1();
+					System.out.println("------------ CLOSE -----------");
+				}
+				System.out.println();
+				break;
+			case 5: // 기록 저장 
+				for (int cls = 0; cls < 15; cls++) { // 화면클리어 
+					System.out.println(); // 화면 클리어 
+					}
+				System.out.println("사용자의 운영체제를 파악중입니다.");
+				os = System.getProperty(
+						"os.name").toLowerCase(); 
+				if (os.contains("win")) {
+					System.out.println("사용자의 운영체제는 Windows입니다.");
+					me.folderWindows(); // 폴더 판별 함수 호출 
+					System.out.println();
+					me.memo2(); // 윈도우 메모 
+				} else if (os.contains("mac")) {
+					System.out.println("사용자의 운영체제는 Mac OS입니다.");
+					me.folderMac();
+					me.memo1();
+				}
+				break;
+			case 6: // 기록 삭제
+				for (int cls = 0; cls < 15; cls ++) {
+					System.out.println();
+				}
+				System.out.println("시스템 운영체제를 파악하고 있습니다...");
+				os = System.getProperty(
+						"os.name").toLowerCase(); 
+				if (os.contains("win")) {
+					System.out.println("사용자의 운영체제는 Windows입니다."
+							+ "\n\t*** Warning ***");
+					System.out.println("*** 진행하면 되돌릴 수 없습니다. 그래도 진행하시겠습니까? ***");
+					System.out.println("\t Y / N "); // YES Next <-> No Previous
+					String a = sc.nextLine();
+					if ( a.equals(y) || a.equals(Y) ) {
+						me.memoDeleteWin(); //메모 삭제 함수 진행
+						}  
+					  else if ( a.equals(n) || a.equals(N) ) {
+						System.out.println("삭제를 중지합니다.");
+						break;
+					} else {
+						System.out.println("다시 입력하세요.");
+						break;
+					}
+				} else if (os.contains("mac")) {
+					System.out.println("사용자의 운영체제는 Mac OS입니다.\n\t *** Warning ***");
+					System.out.println("*** 진행하면 되돌릴 수 없습니다. 그래도 진행하시겠습니까? ***");
+					System.out.println("\t Y / N "); // YES Next <-> No Previous
+					String a = sc.nextLine();
+					if ( a.equals(y) || a.equals(Y) ) {
+						System.out.println(" *** LAST WARNING ***\n    정말 삭제합니까?\n 입력 >");
+						a = sc.nextLine();
+						if ( a.equals(y) || a.equals(Y) ) {
+							me.memoDeleteMac();//메모 삭제 함수 진행
+						} else { 
+							System.out.println("삭제를 중지합니다.");
+							break;
+						}
+					} else if ( a.equals(n) || a.equals(N) ) {
+						System.out.println("삭제를 중지합니다.");
+						break;
+					} else {
+						System.out.println("다시 입력하세요.");
+						break;
+					}
+				}
+				break;
+			case 7: // 클리어
+				for(int i = 0; i < 25; i++ ) {
+					System.out.println();
+				}
+				break;
+			case 0:
+				for (int cls = 0; cls < 15; cls++) {
+					System.out.println(); // 화면 클리어 
+					}
+				cnd = false; //   종료지점
+				System.out.println(" 프로그램을 종료합니다. ");
+				break;
+			
+			default :
+				for (int cls = 0; cls < 15; cls++) {
+					System.out.println(); // 화면 클리어 
+					}
+				System.out.println("WARNING :: 잘못된 입력입니다. 다시 입력하세요 !! ");
+			}	
+		}
+		
+	}
+}
---
+++ 운동관리 프로젝트1.5/src/Memo.java
@@ -0,0 +1,193 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Memo {
+	// 
+	Scanner sc = new Scanner(System.in);
+	int day,time; // 날짜와 운동한 시간이 필요  
+
+	public Memo() {
+		super();
+	}
+	//	--------------------------------------------------------------
+	public void memo1() {  // mac 버전 
+
+		String txt;     // 기록용 변수 선언
+		System.out.print("기록하실 내용을 입력하세요 : ");
+		txt = sc.nextLine(); // 기록을 하기 위한 입력값
+		String fileName = "/Users/iyu/Desktop/GitHub Project/Project//DiaryMac.txt";
+		try{
+			// BufferedWriter / FileWriter를 조합 사용 (속도 향상)
+			BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, true));
+			fw.write(txt);
+			fw.write("\n"); // 줄바꿈 !! 중요!★ unix기반은 \n만 하면됨 
+			fw.flush(); // stream에 남아있는 데이터를 강제로 내보내기 
+			// ex) 수도꼭지 잠그면 호스에 물이 고여있는 것을 빼내기 위해 들어올리는 작업같은 느낌
+			fw.close();
+		} catch(Exception e){
+			e.printStackTrace();
+		} 
+		System.out.println();
+	}
+
+	public void memo2() {  // windows 버전 
+
+		String txt;     // 기록용 변수 선언
+		String fileName = ("C:\\hoon\\WorkOut_Diary.txt");
+
+		System.out.print("기록하실 내용을 입력하세요 : ");
+		txt = sc.nextLine(); // 기록을 하기 위한 입력값
+		try{
+			// BufferedWriter / FileWriter를 조합 사용 (속도 향상)
+			BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, true));
+			fw.write(txt);
+			fw.write("\r\n"); // 줄바꿈 !! windows는 개행 처리 해줘야함
+			fw.flush(); // stream에 남아있는 데이터를 강제로 내보내기 
+			// ex) 수도꼭지 잠그면 호스에 물이 고여있는 것을 빼내기 위해 들어올리는 작업같은 느낌
+			fw.close();
+		} catch(Exception e){
+			e.printStackTrace();// 에러 메시지 발생의 근원지를 찾아 메시지 출력
+		} 
+		System.out.println();
+	}
+	//	--------------------------------------------------------------
+	public void folderMac() { // 폴더 확인  
+		String path = "/Users/iyu/Desktop/GitHub Project/Project//DiaryMac.txt"; // 폴더 경로 선언
+		File Folder = new File(path);// 해당 디렉토리가 없을경우 디렉토리를 생성
+		if (!Folder.exists()) {
+			try {
+				Folder.mkdirs(); // 폴더 생성 midir은 써보니까 마지막 폴더를 가기위한 폴더가 이미 있어야함
+				//			     mkdirs는 그 폴더를 가기위한 모든 폴더를 만들어줌
+				System.out.println("현재 디렉토리가  올바르지 않아 새로운 폴더가 생성되었습니다.");
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} else {
+			System.out.println("이미 폴더가 생성되어 있습니다.");
+		}
+		// 폴더가 제대로 있는지 판별부터 하고 난 뒤, 기록을 진행
+	}
+	public void folderWindows() { // 폴더 확인 클래스함수
+		String path = "C:\\hoon\\"; // 폴더 경로 선언
+		File Folder = new File(path);// 해당 디렉토리가 없을경우 디렉토리를 생성
+		if (!Folder.exists()) {
+			try {
+				Folder.mkdirs();
+				System.out.println("현재 디렉토리가 올바르지 않아 새로운 폴더가 생성되었습니다.");
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} else {
+			System.out.println("이미 폴더가 생성되어 있습니다.");
+		}
+	}
+	//	--------------------------------------------------------------
+	public void memoView1() { // mac 조회  
+		System.out.println("--------MAC Diary OPEN-------");
+		try{ // 예외 처리
+			//파일 객체 생성
+			File file = new File("/Users/iyu/Desktop/GitHub Project/Project//DiaryMac.txt"); // MAC버전
+			//스캐너로 파일 읽기
+			Scanner scan = new Scanner(file);
+			while(scan.hasNextLine()){
+				System.out.println(scan.nextLine());
+			}
+		}catch (FileNotFoundException e) {
+			// TODO: handle exception
+		}
+	}
+	public void memoView2() { // Windows 여기가 문제여 여기가 어휴 ..
+		System.out.println("--------Windows Diary OPEN-------");
+		try{ // 예외 처리
+			//파일 객체 생성
+			File file = new File("C:\\hoon\\WorkOut_Diary.txt"); // windows 버전          
+			System.out.println("Directory = C:\\hoon\\WorkOut_Diary.txt");
+			//스캐너로 파일 조회  
+			Scanner scan = new Scanner(file);
+			while(scan.hasNextLine()){
+				System.out.println(scan.nextLine());
+			} 	
+		} catch (FileNotFoundException e) {
+			// TODO: handle exception
+		}
+		
+	}
+	//	---------------------------------------------------------------
+	public void memoDeleteMac() { // Mac
+		System.out.println("--------MAC TXT DELETE --------");
+		File file = new File("/Users/iyu/Desktop/GitHub Project/Project//DiaryMac.txt"); 
+		if( file.exists() ) {  // 파일이 존재하는가~~ 
+			if(file.isDirectory()) { // 파일 경로 맞는지 확인  
+				File[] files = file.listFiles(); 
+				for( int i=0; i<files.length; i++) { 
+					if( files[i].delete() ) { 
+						System.out.println(files[i].getName()+" 삭제 성공"); 
+					} else { 
+						System.out.println(files[i].getName()+" 삭제 실패"); 
+						System.out.println("HINT => 프로그램을 종료 후 다시 시도 하십시오.");
+					} 
+				} 
+			} if(file.delete()) {  // 파일이 삭제 됐다면 
+				System.out.println("파일 삭제 성 공"); 
+			} else { 
+				System.out.println("ERROR ** 파일 삭제 실패"); 
+				System.out.println("HINT => 프로그램을 종료 후 다시 시도 하십시오.");
+			} 
+		} else { 
+			System.out.println("파일이 존재하지 않습니다."); 
+		}
+		System.out.println("-------- DELETE END --------");
+	}
+	// File의 Delete() 메소드를 이용하여 해당위치 파일을 삭제 시켜 버리기
+	public void memoDeleteWin()   { // Windows 
+		System.out.println("--------Windows TXT DELETE --------");
+		File file = new File("C:\\hoon\\WorkOut_Diary.txt"); 
+		if( file.exists() ) {  // 파일이 존재하는가~~ 
+			if(file.isDirectory()) { // 파일 경로 맞는지 확인  
+				File[] files = file.listFiles(); 
+				for( int i=0; i<files.length; i++) {  // 
+					if( files[i].delete() ) {  // 
+						System.out.println(files[i].getName()+" 삭제 성공"); 
+					} else { 
+						System.out.println(files[i].getName()+" 삭제 실패"); 
+						System.out.println("HINT => 프로그램을 종료 후 다시 시도 하십시오.");
+					} 
+				} 
+			} if(file.delete()) {  // 파일이 삭제 됐다면 
+				System.out.println("파일 삭제 성공"); 
+			} else { 
+				System.out.println("ERROR ** 파일 삭제 실패 **");
+				System.out.println("HINT => 프로그램을 종료 후 다시 시도 하십시오.");
+			} 
+		} else { 
+			System.out.println("파일이 존재하지 않습니다."); 
+		}
+		System.out.println("-------- DELETE END --------");
+	}
+	//	--------------------------------------------------------------
+// public void memoDeleteWin() {
+//	File file = new File("C:\\hoon\\WorkOut_Diary.txt");
+//	System.out.println("--------Windows TXT DELETE --------");
+//    //파일이 삭제될때 까지 반복 (반복횟수 100번)
+//	System.out.println("파일 삭제를 진행합니다. 상황에 따라 시간이 지연 될 수 있습니다.");
+//    for(int i=0; i<100; i++) { 
+//        if(file.delete()) {
+//            System.out.println("파일 삭제를 성공적으로 마쳤습니다.");
+//            break;
+//        } else {
+//            System.out.println("파일 삭제를 실패 하였습니다.. 삭제 시도를 다시 시도 합니다.");
+//            //파일 삭제를 실패한 경우 1초후 다시 실행
+//            try {
+//                Thread.sleep(1000); // 쓰레드 슬립 , 상태제어, 주어진 시간동안 일시정지 
+//            } catch(InterruptedException e) {
+//                e.printStackTrace(); 
+//            }
+//        }
+//    }
+//    System.out.println("-------- DELETE END --------");
+//  }
+}
+	
