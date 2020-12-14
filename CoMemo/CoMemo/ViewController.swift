ba4600c49c589f56013f826aa13009a4fab84dd2
Eight-Corner <corner3499@gmail.com>
2020-12-14 19:10:17 +0900

    📱 IOS Memo App

---
+++ CoMemo/CoMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoMemo
+//
+//  Created by 코너 on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
